f3cdb8c25ac8bf38cfa37468f4cb38798fa0fc8f
Kunal Desai <kunaljaydesai@gmail.com>
2015-10-27 21:45:57 -0700

    turning

--- Micromouse.ino
+++ Micromouse.ino
@@ -1,7 +1,7 @@
-#define right_wheel_1 A6
+#define right_wheel_1 A9
 #define right_wheel_2 A7
 #define left_wheel_1 A8
-#define left_wheel_2 A9
+#define left_wheel_2 A6
 
 
 void right_forward(){
@@ -11,16 +11,45 @@ void right_forward(){
   }
 void left_forward(){
     analogWrite(left_wheel_1, 0);
-    analogWrite(right_wheel_2, 255);
+    analogWrite(left_wheel_2, 255);
     Serial.println("left forward");
+  }
+
+void right_backward(){
+  analogWrite(right_wheel_1, 255);
+  analogWrite(right_wheel_2, 0);
+  Serial.println("right backward");
+  }
+void left_backward(){
+  analogWrite(left_wheel_1, 255);
+  analogWrite(left_wheel_2, 0);
+  Serial.println("left backward");
+  }
+
+void turn_left(){
+  right_forward();
+  left_backward();
+  }
+
+void turn_right(){
+    right_backward();
+    left_forward();
   }
 void setup() {
 
 }
 
 void loop() {
+  /*right_backward();
+  left_backward();
+  delay(5000);
   right_forward();
   left_forward();
+  delay(5000);*/
+  turn_right();
+  delay(5000);
+  turn_left();
+  delay(5000);
 }
 
 
